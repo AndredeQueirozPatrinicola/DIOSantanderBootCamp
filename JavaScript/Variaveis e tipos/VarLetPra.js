75e69368f6759667f46ef1045c6c04877ee9ac5e
unknown <andrepatrinicola@gmail.com>
2022-06-13 18:45:47 -0300

    Aula de Var e Let

---
+++ JavaScript/Variaveis e tipos/VarLetPra.js
@@ -0,0 +1,19 @@
+/*
+
+numberOne = 1;
+
+console.log(numberOne + 2);
+
+*/
+
+var firstName = "André";
+var lastName = "Souza";
+
+if (firstName === "André"){
+    var firstName = "Pedro";
+    let lastName = "Silva";
+}
+
+console.log(firstName, lastName)
+
+/* Dar prioridade ao Let do que o Var. Pensar na logica Local */
