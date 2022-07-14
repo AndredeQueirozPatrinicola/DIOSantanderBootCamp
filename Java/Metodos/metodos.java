283c971867777199a5709590c8c1eb2d50223067
André de Queiroz <andrepatrinicola@gmail.com>
2022-07-13 22:30:23 -0300

    Adicionando ex1 de metodos

---
+++ Java/Metodos/metodos.java
@@ -0,0 +1,27 @@
+public class metodos {
+	public static void soma(double numero1, double numero2) {
+		
+		double resultado = numero1 + numero2;
+				
+		System.out.println("O resultado é: " + resultado);		
+	}
+
+	public static void subtração(double numero1, double numero2) {
+		double resultado = numero1 - numero2;
+		
+		System.out.println("O resultado é: "+ resultado);
+	}
+
+	public static void multiplicacao(double numero1, double numero2) {
+		double resultado = numero1 * numero2;
+		
+		System.out.println("O resultado é: "+resultado);
+	}
+	
+	public static void divisao(double numero1, double numero2) {
+		double resultado = numero1 / numero2;
+		
+		System.out.println("O resultado é: "+resultado);
+	}
+}
+
